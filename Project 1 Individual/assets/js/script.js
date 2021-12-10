2a464d06ec98d2b090d7b69494bc7ead88d19c5a
zlfahmi <z.fahmi312@gmail.com>
2021-12-10 16:59:15 +0800

    Add lightbox javascript

--- Project 1 Individual/assets/js/script.js
+++ Project 1 Individual/assets/js/script.js
@@ -1,28 +1,48 @@
 function validateForm() {
-  let name = document.forms["feedback"]["name"].value;
+  var name = document.forms["feedback"]["name"].value;
   if (name == "") {
-    alert("Name must be filled out");
+    alert("Please fill in the name field");
     return false;
   }
-  let gender = document.forms["feedback"]["gender"].value;
+  var gender = document.forms["feedback"]["gender"].value;
   if (gender == "") {
     alert("Gender must be ticked");
     return false;
   }
-  let subject = document.forms["feedback"]["subject"].value;
+  var subject = document.forms["feedback"]["subject"].value;
   if (subject == "") {
-    alert("Subject must be filled out");
+    alert("Please fill in the subject field");
     return false;
   }
-  let email = document.forms["feedback"]["email"].value;
+  var email = document.forms["feedback"]["email"].value;
   if (email == "") {
-    alert("Email must be filled out");
+    alert("Please fill in the email field");
+    return false;
+  }
+  var message = document.forms["feedback"]["message"].value;
+  if (message == "") {
+    alert("Please fill in the message field");
     return false;
   }
 }
-  let message = document.forms["feedback"]["message"].value;
-  if (message == "") {
-    alert("Message must be filled out");
-    return false;
-  }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
 }
