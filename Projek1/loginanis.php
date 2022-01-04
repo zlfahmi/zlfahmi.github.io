686c13f59883888646b39ebf84b7ae26c5e6c2a0
zlfahmi <z.fahmi312@gmail.com>
2022-01-04 23:21:48 +0800

    Update loginanis.php

--- Projek1/loginanis.php
+++ Projek1/loginanis.php
@@ -34,43 +34,48 @@
    
 ?>
 <!DOCTYPE html>
-<html lang = "en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Login</title>
-        <link rel ="stylesheet" href="css/boostrap.css">
-        <style type="text/css">
-            body {
-                font: 14px sans-serif;
-            }
-            .wrapper {
-                width: 35@px;
-                padding: 2@px;
-            }
-        </style>
-    </head>
-    <body>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <style type="text/css">
+        body {
+            font: 14px sans-serif;
+        }
+
+        .wrapper {
+            width: 35@px;
+            padding: 2@px;
+        }
+
+    </style>
+</head>
+
+<body>
     <?php $confirm_password=""; $confirm_password_err=""; $username_err="";
         $password_err=""; $username=""; ?>
-        <div class="wrapper">
-            <h2>Login Page</h2>
-            <p>Please fill this form to login an account</p>
-            <form action="<?php echo htmlspecialchars($_SERVER["php_self"]);?>" method="POST">
-               <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?> ">
+    <div class="wrapper">
+        <h2>Login Page</h2>
+        <p>Please fill this form to login an account</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?> ">
                 <label>Username</label>
                 <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
-                </div>
-                <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?> ">
+            </div>
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?> ">
                 <label>Password</label>
                 <input type="text" name="password" class="form-control" value="<?php echo $password; ?>">
-                </div>
-                <br><br>
-                <div class="form-group">
-                    <input type="submit"class="btn btn-primary" value="Submit">
-                    <input type="reset" class="btn btn-default" value="Reset">
-                </div>
-            </form>
-            <p>Don't have an account ? <a href ="register.php">Register Here</a>.</p>
-        </div>
-    </body>
+            </div>
+            <br><br>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Submit">
+                <input type="reset" class="btn btn-default" value="Reset">
+            </div>
+        </form>
+        <p>Don't have an account ? <a href="register.php">Register Here</a>.</p>
+    </div>
+</body>
+
 </html>
